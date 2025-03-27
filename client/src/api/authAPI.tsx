886ac66e587daae8820737610ba1f5f05578dc0e
Leland Mitchell <leland.a.mitchell2@gmail.com>
2025-03-27 09:26:01 -0400

    created a post request for the client for login

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,8 +2,25 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+    if (!response.ok) {
+      const errorMessage = await response.json();
+      throw new Error(errorMessage.message); 
+    }
+
+    const token = await response.json();
+    return token;
+  } catch (error) {
+    console.log('login error', error);
+    return Promise.reject('Could not log in');
+  }
 }
-
-
 
 export { login };
