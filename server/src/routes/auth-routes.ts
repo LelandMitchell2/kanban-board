00a0356f29abcbc13e2057dd734f3e7bdd0c33e2
Leland Mitchell <leland.a.mitchell2@gmail.com>
2025-04-07 19:31:01 -0400

    fixed some crashes and CSS style

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -4,17 +4,17 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+  // If the user exists and the password is correct, return a JWT token
   const { username, password } = req.body;
   const user = await User.findOne({  where: {username} });
 
   if (!user) {
-    return res.status(401).json({ message: 'Invalid username or password' });
+    return res.status(401).json({ message: 'Invalid username' });
   }
 
   const isValidPassword = await bcrypt.compare(password, user.password);
   if (!isValidPassword) {
-    return res.status(401).json({ message: 'Invalid username or password' });
+    return res.status(401).json({ message: 'Invalid password' });
   }
 
   const secretKey = process.env.JWT_SECRET_KEY || '';
@@ -29,3 +29,4 @@ const router = Router();
 router.post('/login', login);
 
 export default router;
+
